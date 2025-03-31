bb00722b49eb66ec01b4a0cfd11edbd64ae45917
Michal Cichowski <mike@silentmike.pl>
2025-03-31 16:34:05 +0200

    up

--- src/SilentMike.OldPhonePad.Application/Commons/Interfaces/IPhonePadService.cs
+++ src/SilentMike.OldPhonePad.Application/Commons/Interfaces/IPhonePadService.cs
@@ -6,12 +6,12 @@ namespace SilentMike.OldPhonePad.Application.Commons.Interfaces;
 public interface IPhonePadService
 {
     /// <summary>
-    ///     Decodes a sequence of keypad presses into text.
+    ///     Converts a sequence of keypad presses into text.
     /// </summary>
-    /// <param name="input">Input sequence to be decoded.</param>
-    /// <param name="keyMapStrategy">Strategy that should be used to decoded input.</param>
+    /// <param name="input">Input sequence to be convert.</param>
+    /// <param name="keyMapStrategy">Strategy that should be used to converted input.</param>
     /// <param name="skipInvalidChars">Skip invalid character or throw exception</param>
-    /// <returns>The decoded text string. Returns empty string if input is null or empty.</returns>
+    /// <returns>The converted text string. Returns empty string if input is null or empty.</returns>
     /// <exception cref="SequenceInvalidEndException">Thrown when the input contains invalid end of sequence.</exception>
     /// <exception cref="SequenceInvalidCharException">Thrown when the input contains invalid characters.</exception>
     string Convert(string input, IKeyMapStrategy keyMapStrategy, bool skipInvalidChars);
